5ee42157986880f54c7e0a53661a766265eeceeb
Simon <34153549+seIncorp@users.noreply.github.com>
2021-11-10 14:15:16 +0100

    More examples

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* av[])
 	/******************** 1 *************************/
 	// Old style
 	std::cout << "Old style (Example 1)" << std::endl;
+
 	// New style
 	o << "New style (Example 1)" << e;
 	/************************************************/
@@ -15,6 +16,7 @@ int main(int argc, char* av[])
 	// Old style
 	std::cout << "Old style (Example 2)";
 	std::cout << std::endl;
+
 	// New style
 	o << "New style (Example 2)";
 	ee;
@@ -23,6 +25,7 @@ int main(int argc, char* av[])
 	/******************** 3 *************************/
 	// Old style
 	std::vector<int> test_1;
+
 	// New style
 	Vi test_2; // for integers
 	Vd test_3; // for doubles
@@ -30,4 +33,15 @@ int main(int argc, char* av[])
 	Vc test_5; // for characters
 	/************************************************/
 
+	/******************** 3 *************************/
+	// Old style
+	std::cout << std::boolalpha;
+	std::cout << (1 == 2) << std::endl;
+	std::cout << std::noboolalpha;
+
+	// New style
+	BAon;
+	o << (1 == 2) << e;
+	BAoff;
+	/************************************************/
 }
