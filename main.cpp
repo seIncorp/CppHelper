532836a4ec4456127d3890f799e3456a5bf5bf2e
Simon <34153549+seIncorp@users.noreply.github.com>
2021-11-10 13:34:41 +0100

    Some examples.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 
 int main(int argc, char* av[])
 {
+	/* EXAMPLES */
 
+	/******************** 1 *************************/
+	// Old style
+	std::cout << "Old style (Example 1)" << std::endl;
+	// New style
+	o << "New style (Example 1)" << e;
+	/************************************************/
 
+	/******************** 2 *************************/
+	// Old style
+	std::cout << "Old style (Example 2)";
+	std::cout << std::endl;
+	// New style
+	o << "New style (Example 2)";
+	ee;
+	/************************************************/
 
 }
